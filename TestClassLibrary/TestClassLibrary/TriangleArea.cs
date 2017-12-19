df8e0582ccfe1698538fb081256a033d53d0c069
yurchevv <vladislav.yurchev@yandex.ru>
2017-12-19 21:00:05 +0300

    Initial commit

---
+++ TestClassLibrary/TestClassLibrary/TriangleArea.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassLibrary
+{
+    public class Triangle
+    {
+        #region Fields
+        private double _firstSide;
+        private double _secondSide;
+        private double _thirdSide;
+        private double halfPerimeter;
+        #endregion
+
+        public double FirstSide
+        {
+            set
+            {
+                _firstSide = value;
+            }
+            
+            get
+            {
+                return _firstSide;
+            }
+        }
+
+        public double SecondSide
+        {
+            set
+            {
+                _secondSide = value;
+            }
+
+            get
+            {
+                return _secondSide;
+            }
+        }
+
+        public double ThirdSide
+        {
+            set
+            {
+                _thirdSide = value;
+            }
+
+            get
+            {
+                return _thirdSide;
+            }
+        }
+
+        //Method calculating triangle's area
+        public double? GetTriangleArea()
+        {
+            double halfPerimeter = GetHalfPerimeter();
+
+            if (halfPerimeter != 0)
+            {
+                double area = Math.Sqrt((halfPerimeter) * (halfPerimeter - _firstSide) * (halfPerimeter - _secondSide) * (halfPerimeter - _thirdSide));
+            }
+
+            return null;
+        }
+
+        //Method calculating triangle's half-perimeter
+        public double GetHalfPerimeter()
+        {
+            if ((_firstSide < 0) | (_secondSide < 0) | (_thirdSide < 0))
+            {
+                return 0;
+            }
+
+            halfPerimeter = (_firstSide + _secondSide + _thirdSide) / 2;
+
+            return halfPerimeter;
+        }
+    }
+}
---
+++ TestClassLibrary/TestUnitTestProject/UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestClassLibrary;
+
+namespace TestUnitTestProject
+{
+    [TestClass]
+    public class TriangleAreaTest
+    {
+        [TestMethod]
+        public void GetAreaNotEqualStandart()
+        {
+            Triangle triangle = new Triangle();
+
+            triangle.FirstSide = 5;
+
+            triangle.SecondSide = 10;
+
+            triangle.ThirdSide = 10;
+
+            double area = triangle.GetHalfPerimeter();
+
+            Assert.AreNotEqual(1, area);
+        }
+
+        [TestMethod]
+        public void GetAreaNotNull()
+        {
+            Triangle triangle = new Triangle();
+
+            triangle.FirstSide = 5;
+
+            triangle.SecondSide = 10;
+
+            triangle.ThirdSide = 10;
+
+            double area = triangle.GetHalfPerimeter();
+
+            Assert.AreNotEqual(null, area);
+        }
+
+        [TestMethod]
+        public void GetAreaEqualStandart()
+        {
+            Triangle triangle = new Triangle();
+
+            triangle.FirstSide = 3;
+
+            triangle.SecondSide = 4;
+
+            triangle.ThirdSide = 5;
+
+            double area = triangle.GetHalfPerimeter();
+
+            Assert.AreEqual(6, area);
+        }
+
+        [TestMethod]
+        public void GetAreaIsPositive()
+        {
+            Triangle triangle = new Triangle();
+
+            triangle.FirstSide = -3;
+
+            triangle.SecondSide = -4;
+
+            triangle.ThirdSide = -5;
+
+            double area = triangle.GetHalfPerimeter();
+
+            Assert.AreEqual(0, area);
+        }
+    }
+}
